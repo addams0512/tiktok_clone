e34c430d82515349c8c5aa2e7404161690bb07c9
NguyenNgocQuangMinh <quangminh0512@icloud.com>
2023-04-16 14:54:58 +0700

    Tiktok_clone

---
+++ backend_data/schemas/index.ts
@@ -0,0 +1,6 @@
+import post from './post'
+import postedBy from './postedBy'
+import user from './user'
+import comment from './comment'
+
+export const schemaTypes = [user, post, comment, postedBy]
